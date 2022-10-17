ced464436664cfbf7d72a8c4583bdbf8fb93c69a
Oliver Pile <o.pile@uos.ac.uk>
2022-10-17 16:41:54 +0100

    Makes file output more readable

--- ItemFileHandler.java
+++ ItemFileHandler.java
@@ -10,7 +10,8 @@ public class ItemFileHandler {
             Scanner reader = new Scanner(itemFile);
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-                System.out.printf("Line = %s%n", line);
+                String[] splitLine = line.split(",");
+                System.out.printf("ID: %s, Description: %s, Unit Price: %s, Quantity in Stock: %s, Total price: %s%n",splitLine[0],splitLine[1],splitLine[2],splitLine[3],splitLine[4]);
             }
             reader.close();
 
