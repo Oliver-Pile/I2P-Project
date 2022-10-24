ee902892ca227b14db1bab5f41886ab838fc7490
Oliver Pile <o.pile@uos.ac.uk>
2022-10-24 14:22:07 +0100

    Modify read method ot return list of lines

--- src/ItemFileHandler.java
+++ src/ItemFileHandler.java
@@ -1,24 +1,29 @@
 package src;
 
 import java.io.*;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ItemFileHandler {
     private final File itemFile = new File("src/items.txt");
 
 
-    public void read() {
+    public LinkedList<String[]> readLines() {
         try {
+            LinkedList<String[]> lines = new LinkedList<>();
             Scanner reader = new Scanner(itemFile);
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
                 String[] splitLine = line.split(",");
-                System.out.printf("ID: %s, Description: %s, Unit Price: %s, Quantity in Stock: %s, Total price: %s%n",splitLine[0],splitLine[1],splitLine[2],splitLine[3],splitLine[4]);
+                lines.add(splitLine);
+                //System.out.printf("ID: %s, Description: %s, Unit Price: %s, Quantity in Stock: %s, Total price: %s%n",splitLine[0],splitLine[1],splitLine[2],splitLine[3],splitLine[4]);
             }
             reader.close();
+            return lines;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            return null;
         }
 
     }
