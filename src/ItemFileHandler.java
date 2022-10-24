561d4d9aa0faec4693a9ac235bd4bdebd013cabb
Oliver Pile <o.pile@uos.ac.uk>
2022-10-24 14:34:18 +0100

    Fix output file option due to refactoring

--- src/ItemFileHandler.java
+++ src/ItemFileHandler.java
@@ -16,7 +16,7 @@ public class ItemFileHandler {
                 String line = reader.nextLine();
                 String[] splitLine = line.split(",");
                 lines.add(splitLine);
-                //System.out.printf("ID: %s, Description: %s, Unit Price: %s, Quantity in Stock: %s, Total price: %s%n",splitLine[0],splitLine[1],splitLine[2],splitLine[3],splitLine[4]);
+
             }
             reader.close();
             return lines;
--- src/store.java
+++ src/store.java
@@ -69,7 +69,10 @@ public class store {
 					break;
 				case 5:
 					System.out.println("Item File");
-					LinkedList<String[]> newLines = itemFileHandler.readLines();
+					LinkedList<String[]> readLines = itemFileHandler.readLines();
+					for (String[] splitLine : readLines){
+						System.out.printf("ID: %s, Description: %s, Unit Price: %s, Quantity in Stock: %s, Total price: %s%n",splitLine[0],splitLine[1],splitLine[2],splitLine[3],splitLine[4]);
+					}
 					break;
 				default:
 					System.out.println("Incorrect input, please try again");
