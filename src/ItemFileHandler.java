6c9f02b582e60e277e4e47520b4566363d32c5d5
Oliver Pile <o.pile@uos.ac.uk>
2022-10-31 12:25:48 +0000

    Pseudocode for update method

--- src/ItemFileHandler.java
+++ src/ItemFileHandler.java
@@ -37,4 +37,16 @@ public class ItemFileHandler {
             return false;
         }
     }
+
+    public void update(){
+        /*
+        Param: New item object with updated params
+        Get current lines
+        For each line
+            Check if line ID == ID of current Item
+            If no, write line to new file (temp file then rename)
+            If yes, write new item object (from params) into new file
+        Once all lines complete, rename temp file to items.txt to complete.
+         */
+    }
 }
