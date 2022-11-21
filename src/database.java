7c050bb075ed84ba7a9b5b4f51e48671a43be584
Oliver Pile <oliver.pile@bt.com>
2022-11-21 16:16:37 +0000

    Add CRUD methods to interact with DB

--- src/database.java
+++ src/database.java
@@ -1,29 +1,46 @@
 package src;
 import java.sql.*;
+import java.util.LinkedList;
 
 public class database {
     static Connection con;
     static Statement st;
-    public static void main(String[] args) throws Exception {
-        //Registering the Driver
-        //DriverManager.registerDriver(new org.sqlite.JDBC());
-        //Getting the connection
+
+    public database() throws SQLException {
         String url = "jdbc:sqlite:src/storeDB.db";
         con = DriverManager.getConnection(url);
         System.out.println("Connection established......");
         st = con.createStatement();
         createTable();
-        insert();
     }
 
-    private static void createTable() throws SQLException {
+    private void createTable() throws SQLException {
         String toEx = "CREATE TABLE IF NOT EXISTS Items (itemID INTEGER PRIMARY KEY AUTOINCREMENT, desc TEXT NOT NULL, price REAL NOT NULL, quantity INTEGER NOT NULL, totalPrice REAL NOT NULL)";
         st.execute(toEx);
     }
-
-    private static void insert() throws SQLException {
-        String toEX = "INSERT INTO Items (desc, price, quantity, totalPrice) values('Test item number 2', 8, 100, 800)";
-        st.execute(toEX);
+    public int add(String toInsert) throws SQLException {
+        st.execute(toInsert);
+        ResultSet rs =st.executeQuery("SELECT last_insert_rowid()");
+        return rs.getInt("last_insert_rowid()");
     }
 
+    public void update(Item item) throws SQLException {
+        String toUpdate = String.format("UPDATE Items SET quantity=%d WHERE itemID=%d",item.getQuantity(),item.getID());
+        st.execute(toUpdate);
+    }
+    public void delete(Item item){
+        String toDelete = String.format("DELETE from Items WHERE itemID=%d",item.getID());
+    }
+
+    public LinkedList<Item> getItems() throws SQLException {
+        LinkedList<Item> items = new LinkedList<>();
+        ResultSet allItems = st.executeQuery("SELECT * FROM Items");
+        while (allItems.next()){
+            items.add(new Item(allItems));
+        }
+        return items;
+    }
+    public void close() throws SQLException {
+        con.close();
+    }
 }
