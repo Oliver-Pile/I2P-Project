0a9a5e2c679d6bbb94ba186e5f1312a9b7bfb716
Oliver Pile <oliver.pile@bt.com>
2022-11-21 16:17:24 +0000

    Add constructor for making item from DB. alter getters/setters

--- src/Item.java
+++ src/Item.java
@@ -1,29 +1,38 @@
 package src;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 
 public class Item {
-    private String ID;
+    private int ID;
     private String desc;
     private double price;
     private int quantity;
     private double totalPrice;
 
 
-    public Item(String line){
-        String[] splitLine = line.split(",");
-        this.ID = splitLine[0];
-        this.desc = splitLine[1];
-        this.price = Double.parseDouble(splitLine[2]);
-        this.quantity = Integer.parseInt(splitLine[3]);
-        this.totalPrice = Double.parseDouble(splitLine[4]);
-    }
-    public Item (String ID, String desc, double price, int quantity){
-        this.ID = ID;
+//    public Item(String line){
+//        String[] splitLine = line.split(",");
+//        this.ID = splitLine[0];
+//        this.desc = splitLine[1];
+//        this.price = Double.parseDouble(splitLine[2]);
+//        this.quantity = Integer.parseInt(splitLine[3]);
+//        this.totalPrice = Double.parseDouble(splitLine[4]);
+//    }
+    public Item (String desc, double price, int quantity){
         this.desc = desc;
         this.price = price;
         this.quantity = quantity;
         this.totalPrice = price * quantity;
+    }
+
+    public Item (ResultSet rs) throws SQLException {
+        this.ID = rs.getInt("itemID");
+        this.desc = rs.getString("desc");
+        this.price = rs.getDouble("price");
+        this.quantity = rs.getInt("quantity");
+        this.totalPrice = rs.getDouble("totalPrice");
     }
 
     public String getItemDetails(boolean basic){
@@ -33,26 +42,29 @@ public class Item {
 
     }
     public int getID(){
-        return Integer.parseInt(this.ID);
+        return this.ID;
     }
-    public String getFileString(){
-        return String.format("%s,%s,%.2f,%s,%.2f%n",this.ID,this.desc,this.price,this.quantity,this.totalPrice);
+//    public String getFileString(){
+//        return String.format("%s,%s,%.2f,%s,%.2f%n",this.ID,this.desc,this.price,this.quantity,this.totalPrice);
+//    }
+    public String getSQLAddString(){
+        return String.format("INSERT INTO Items (desc, price, quantity, totalPrice) values('%s', %.2f, %d, %.2f)",this.desc,this.price,this.quantity,this.totalPrice);
     }
 
-    public static String generateItemID(){
-        ItemFileHandler handler = new ItemFileHandler();
-        LinkedList<Item> items = handler.readLines();
-        int largestID = Integer.MIN_VALUE;
-        int currentID = 0;
-        for (Item item : items){
-            currentID = item.getID();
-            if (currentID>largestID)largestID=currentID;
-        }
-        if(currentID<99999) currentID++;
-        else return null;
-        String newID = String.format("%05d",currentID);
-        return newID;
-    }
+//    public static String generateItemID(){
+//        ItemFileHandler handler = new ItemFileHandler();
+//        LinkedList<Item> items = handler.readLines();
+//        int largestID = Integer.MIN_VALUE;
+//        int currentID = 0;
+//        for (Item item : items){
+//            currentID = item.getID();
+//            if (currentID>largestID)largestID=currentID;
+//        }
+//        if(currentID<99999) currentID++;
+//        else return null;
+//        String newID = String.format("%05d",currentID);
+//        return newID;
+//    }
 
     public void changeQuantity(int newQuantity){
         this.quantity = newQuantity;
@@ -69,5 +81,8 @@ public class Item {
     }
     public int getQuantity(){
         return this.quantity;
+    }
+    public void setID(int id){
+        this.ID = id;
     }
 }
