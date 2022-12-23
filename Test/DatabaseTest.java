733e4583b43b48812bacf79e268c37ee0537805f
Oliver Pile <o.pile@uos.ac.uk>
2022-12-23 14:56:05 +0000

    Add delete unit test with random desc generator

--- Test/DatabaseTest.java
+++ Test/DatabaseTest.java
@@ -4,12 +4,9 @@ import org.junit.Test;
 import src.Item;
 import src.Database;
 
-import java.io.ByteArrayInputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -21,11 +18,19 @@ public class DatabaseTest {
         return (Item) items.getLast();
 
     }
+    private String getRandomDesc(){
+        String randomStr = "";
+        Random r = new Random();
+        for(int i=0;i<10;i++){
+            randomStr += (char)(r.nextInt(26) + 'a');
+        }
+        return randomStr;
+    }
 
     @Test
     public void addTest() throws SQLException {
         Database db = new Database("testDB");
-        Item newItem = new Item("A unit test item for adding",10.99,100);
+        Item newItem = new Item(getRandomDesc(),10.99,100);
         db.add(newItem);
         Item lastItem = getLastDB(db);
         assertEquals(lastItem.getDesc(),newItem.getDesc());
@@ -36,9 +41,11 @@ public class DatabaseTest {
 
     @Test
     public void updateTest() throws SQLException {
+        //Setting up DB so there is an item to update
         Database db = new Database("testDB");
-        Item newItem = new Item("A unit test item for updating",10.99,100);
+        Item newItem = new Item(getRandomDesc(),10.99,100);
         db.add(newItem);
+        //Updating the item
         Item itemToUpdate = getLastDB(db);
         itemToUpdate.changeQuantity(20);
         db.update(itemToUpdate,20);
@@ -46,5 +53,17 @@ public class DatabaseTest {
         assertEquals(itemToUpdate.getDesc(),updatedItem.getDesc());
         assertEquals(20,updatedItem.getQuantity());
         db.close();
+    }
+
+    @Test
+    public void deleteTest() throws SQLException {
+        //Setting up DB so there is an item to delete
+        Database db = new Database("testDB");
+        Item newItem = new Item(getRandomDesc(),10.99,100);
+        db.add(newItem);
+        Item itemToDelete = getLastDB(db);
+        db.delete(itemToDelete);
+        Item lastItem = getLastDB(db);
+        assertNotEquals(itemToDelete.getDesc(),lastItem.getDesc());
     }
 }
