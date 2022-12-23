e147d5b8ca7f68e18a5fa4a9ab8fe23b95215ed1
Oliver Pile <o.pile@uos.ac.uk>
2022-12-23 14:20:57 +0000

    Add unit test for add on db

---
+++ Test/DatabaseTest.java
@@ -0,0 +1,35 @@
+package Test;
+
+import org.junit.Test;
+import src.Item;
+import src.Database;
+
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+
+public class DatabaseTest {
+
+    private Item getLastDB(Database db) throws SQLException {
+        LinkedList items = db.getItems();
+        return (Item) items.getLast();
+
+    }
+
+    @Test
+    public void addTest() throws SQLException {
+        Database db = new Database("testDB");
+        Item newItem = new Item("A unit test item for adding",10.99,100);
+        db.add(newItem);
+        Item lastItem = getLastDB(db);
+        assertEquals(lastItem.getDesc(),newItem.getDesc());
+        assertEquals(lastItem.getPrice(),newItem.getPrice(),0);
+        assertEquals(lastItem.getQuantity(),newItem.getQuantity());
+    }
+}
